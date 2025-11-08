e078f3544230685c53fe8deac16f6787872c056d
Zaridigi <zaridigi@gmail.com>
2025-11-08 21:43:59 +0300

    Change9-P13

--- Practice13/function.js
+++ Practice13/function.js
@@ -1,4 +1,35 @@
-function summ(a,b) {
-    return a+b;
-  }
+// function summ(a,b) {
+//     return a+b;
+//   }
+//   console.log(summ(3,4));
+//   В js не используются такие функции - так как это долго
+//   Используются стрелочные функции:
+  let summ= (a,b) => a+b;
+  
   console.log(summ(3,4));
+
+    let sqrmy = (a) =>  Math.sqrt(a);
+
+    console.log(sqrmy(4));
+
+//   Class User () {
+// this.a;
+// constructor()
+// console.log(a);
+//   }
+// Массивы
+
+let a= []
+for(let i=0; i<=11; i++) {
+  a[i]=i;
+}
+console.log(a);
+
+// Есть тип объекты
+
+let b={
+name:'Зарина',
+Phone:'855',
+age:33
+}
+console.log(b);
