cdd2752aab984c8beccdf7e8773972e5a6647510
Zaridigi <zaridigi@gmail.com>
2025-11-22 02:27:25 +0300

    j

--- Practise19/src/App.js
+++ Practise19/src/App.js
@@ -37,7 +37,7 @@ function MovingDot() {
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Привет</h1>
       <h2>Start editing to see some magic happen!</h2>
       <MovingDot />
     </div>
