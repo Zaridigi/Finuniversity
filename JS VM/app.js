70d42dbc7bae15565aeff935c75718ba32ebef71
Zaridigi <zaridigi@gmail.com>
2025-12-14 23:41:40 +0300

    js calculator1

---
+++ JS VM/app.js
@@ -0,0 +1,34 @@
+let num = 42
+let firstName = 'Zarina'
+const isProgrammer = true
+
+/*Can do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let _num_ = 12
+let first_name = 'Elena'
+let num42 = 10
+*/
+
+/*Restricted
+let 42num = '11'
+let my-num = 1
+let const
+*/
+
+
+// firstName = "Max"
+// isProgrammer = false // error because of const
+
+// alert(firstName)
+// console.log('Test:', num, isProgrammer)
+
+console.log(num + 10)
+console.log(num - 10)
+console.log(num * 10)
+console.log(num / 10)
+console.log(num)
+
+let num2 = num +10
